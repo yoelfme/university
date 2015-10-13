459a5a1f23a179e3b8a0077cf7df89acef8d4308
Yoel Monzon <yoelfme@hotmail.com>
2015-10-13 16:14:38 -0600

    [add] test for index.js

---
+++ test/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// Load Modules
+
+var Index = require('../lib/index');
+var Version = require('../lib/version');
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var Code = require('code');
+
+var describe = lab.describe;
+var it = lab.it;
+var expect = Code.expect;
+
+describe('Index', function () {
+
+    it('Server start well :)', function (done) {
+
+        Index.init(null, function (err, server) {
+
+            expect(err).to.not.exist();
+            server.stop(done);
+        });
+    });
+
+    it('Server fail :(', { parallel: false }, function (done) {
+
+        Version.register = function (server, options, next) {
+
+            return next(new Error());
+        };
+
+        Version.register.attributes = {
+            name: 'test'
+        };
+
+        Index.init(null, function (err, server) {
+
+            expect(err).to.exist();
+            server.stop(done);
+        });
+    });
+});
