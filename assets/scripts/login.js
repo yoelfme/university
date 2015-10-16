9e6cb2310e79777298d5b824341fb5bd6cdd25ce
Yoel Monzon <yoelfme@hotmail.com>
2015-10-16 13:00:31 -0600

    [add] event to button login

---
+++ assets/scripts/login.js
@@ -0,0 +1,10 @@
+document.onreadystatechange = function () {
+
+    if (document.readyState === 'complete') {
+        document.getElementById('btnLogin').addEventListener('click', function (event) {
+
+            event.preventDefault();
+            console.log('Login button has been clicked');
+        });
+    };
+};
